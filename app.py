a48410c8989a4be6fd1fb0af77c2935a7a590efa
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-04-13 12:43:14 +0000

    запустился base

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-import socketio
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route("/")
+def hello_world():
+    # return "<p>Hello, World!</p>"
+    return render_template('base.html')
+
+# sio = socketio.Server()
+# app = socketio.WSGIApp(sio, static_files={
+#    '/': './public/'
+#})
+if __name__ == '__main__':
+    socketio.run(app)
+  
+
+# @sio.event
+# def connect(sid, environ):
+#     print(sid, 'connected')
 
 
-
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    '/': './public/'
-})
-
-
-@sio.event
-def connect(sid, environ):
-    print(sid, 'connected')
-
-
-@sio.event
-def disconnect(sid):
-    print(sid, 'disconnected')
+# @sio.event
+# def disconnect(sid):
+#     print(sid, 'disconnected')
